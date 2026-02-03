0b02f9013bb309d36debda4cab1cea3a5737aee3
Mohamed Zeid <medozeid5000@gmail.com>
2026-02-03 09:55:45 +0200

    second commit

--- index.js
+++ index.js
@@ -8,6 +8,4 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
