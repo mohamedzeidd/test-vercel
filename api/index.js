5840928b550b57440ed727f66b1afeb14a0ac40b
Mohamed Zeid <medozeid5000@gmail.com>
2026-02-03 10:08:25 +0200

    fdfdfd

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.send("EXPRESS IS WORKING 🚀");
 });
 
 module.exports = app;
